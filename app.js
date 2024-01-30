9c59047d7f6ed2ea0fc1e2368158a5f1e5f20384
hongminyeong <minyung1240@daum.net>
2024-01-30 18:20:07 +0900

    feat:Express를 사용한 초기 서버 설정 구축 과 HTML 구축

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 8001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('views'));
+
+// [라우터 분리]
+const indexRouter = require('./routes');
+app.use('/', indexRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// api
+app.post('/', (req, res) => {
+  res.send('안뇽??');
+});
+
+app.listen(PORT, () => {
+  console.log(`Port ${PORT} is opening`);
+});
