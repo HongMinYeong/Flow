768e2ad04b667839fbce54ab4a4d02132116e4f1
hongminyeong <minyung1240@daum.net>
2024-02-01 17:45:37 +0900

    fix: 환경변수 데이터베이스 연결 정보 업데이트

---
+++ blocking.sql
@@ -0,0 +1,9 @@
+
+create database project CHARACTER set utf8mb4 collate utf8mb4_unicode_ci;
+
+USE project;
+
+CREATE TABLE blocking (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    extension VARCHAR(200) NULL UNIQUE
+);
